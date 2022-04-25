c2878f845fa0f34ff0ecfec5bde360b1060e4481
oheckmann <38300728+oheckmann@users.noreply.github.com>
2022-04-25 12:17:09 -0400

    Update main.go

--- go.mod
+++ go.mod
@@ -3,14 +3,20 @@ module finalProject
 go 1.17
 
 require (
-	golang.org/x/net v0.0.0-20211112202133-69e39bad7dc2
-	golang.org/x/sys v0.0.0-20210615035016-665e8c7367d1
-	golang.org/x/term v0.0.0-20201126162022-7de9c90e9dd1
+	go.mongodb.org/mongo-driver v1.9.0
+	golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4
 )
-
-require golang.org/x/crypto v0.0.0-20220411220226-7b82a4e95df4 // indirect
 
 require (
-	github.com/go-sql-driver/mysql v1.6.0
-	golang.org/x/text v0.3.6 // indirect
+	github.com/go-stack/stack v1.8.0 // indirect
+	github.com/golang/snappy v0.0.1 // indirect
+	github.com/klauspost/compress v1.13.6 // indirect
+	github.com/pkg/errors v0.9.1 // indirect
+	github.com/xdg-go/pbkdf2 v1.0.0 // indirect
+	github.com/xdg-go/scram v1.0.2 // indirect
+	github.com/xdg-go/stringprep v1.0.2 // indirect
+	github.com/youmark/pkcs8 v0.0.0-20181117223130-1be2e3e5546d // indirect
+	golang.org/x/sync v0.0.0-20190911185100-cd5d95a43a6e // indirect
 )
+
+require golang.org/x/text v0.3.6 // indirect
--- main.go
+++ main.go
@@ -1,27 +1,76 @@
 package main
 
 import (
+	"context"
 	"fmt"
 	"log"
 	"net/http"
-	"sync"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt"
 )
 
-var RWLock sync.RWMutex
+const (
+	mongodbEndpoint = "mongodb://172.17.0.2:27017" // Find this from the Mongo container
+)
+
+type Post struct {
+	ID            primitive.ObjectID `bson:"_id"`
+	Username      string             `bson:"username"`
+	Password      string             `bson:"password"`
+	BloodType     string             `bson:"blood_type"`
+	ContactNumber string             `bson:"contact_number"`
+	CreatedAt     time.Time          `bson:"created_at"`
+	Tags          string              `bson:"tags"`
+}
+
+var ctx context.Context
+var col *mongo.Collection
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	// db := database{"shoes": 50, "socks": 5}
+	client, err := mongo.NewClient(
+		options.Client().ApplyURI(mongodbEndpoint),
+	)
+	checkError(err)
+	// Connect to mongo
+	ctx = context.Background()
+	err = client.Connect(ctx)
+	checkError(err)
+	// Disconnect
+	defer client.Disconnect(ctx)
+
+	// select collection from database
+	col = client.Database("bloodBankDatabase").Collection("users")
+
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir("static")))
 	mux.HandleFunc("/loginHandler", loginHandler)
-	mux.HandleFunc("/Register", Register)
 	mux.HandleFunc("/RegisterHandler", RegisterHandler)
-	mux.HandleFunc("/addUser", addUser)
 	mux.HandleFunc("/deleteUser", deleteUser)
 	mux.HandleFunc("/updateUserinfo", updateUserinfo)
 	mux.HandleFunc("/findDonors", findDonors)
-	mux.HandleFunc("listDonors", listDonors)
+	mux.HandleFunc("/listAllDonors", listDonors)
 	log.Fatal(http.ListenAndServe(":8000", mux)) // Listens for curl communication of localhost
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+    err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+    return err == nil
 }
 
 func loginHandler(w http.ResponseWriter, req *http.Request) {
@@ -30,27 +79,97 @@ func loginHandler(w http.ResponseWriter, req *http.Request) {
 	username := req.FormValue("username")
 	password := req.FormValue("password")
 	fmt.Fprintln(w, "username:", username, "password:", password)
-}
 
-// type database map[string]dollars // database of items with their polar prices
-func Register(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "Register Page")
+
+	var hash string
+
+	filter := bson.M{"username": username}
+
+	// find one document
+	var p Post
+	if err := col.FindOne(ctx, filter).Decode(&p); err != nil {
+		fmt.Fprintln(w, "user:",username," is not registered!") // if the item does not exist write and error
+
+	} else {
+		fmt.Printf("post: %+v\n", p)
+		fmt.Fprintln(w, "hashed password of ",username, " : ",p.Password)
+		hash = p.Password
+	}
+
+	err := CheckPasswordHash(password,hash)
+	if err != true {
+		fmt.Fprintln(w, "username or password is incorrect!")
+	}
+	
 }
 
 func RegisterHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, "RegisterHandler Page")
-}
+	req.ParseForm()
+	username := req.FormValue("username")
+	password := req.FormValue("password")
+	bloodType := req.FormValue("bloodType")
+	contactNumber := req.FormValue("contactNumber")
+	fmt.Fprintln(w, "username:", username, "password:", password, "bloodType: ",bloodType, "contactNumber:",contactNumber)
+	hashedPassword, err := HashPassword(password)
+	checkError(err)
 
-func addUser(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "addUser Page")
+	// Insert one
+	res, err := col.InsertOne(ctx, &Post{
+		ID:            primitive.NewObjectID(),
+		Username :     username,          
+		Password :     hashedPassword,            
+		BloodType :    bloodType,   
+		ContactNumber: contactNumber, 
+		CreatedAt:     time.Time,   
+		Tag : "bloodDonors"})
+	checkError(err)
+
+	if err == nil {
+		fmt.Printf("inserted id: %s\n", res.InsertedID.(primitive.ObjectID).Hex())
+	}
+
 }
 
 func deleteUser(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "deleteUser Page")
+	//fmt.Fprintln(w, "deleteUser Page")
+	username := req.FormValue("username")
+	res, err := col.DeleteMany(ctx, bson.M{"Username": username})
+	if err != nil {
+		log.Fatal(err)
+	} else {
+		fmt.Fprintln(w, "delete count: ", res.DeletedID)
+	}
 }
 
 func updateUserinfo(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "updateUserinfo Page")
+	//fmt.Fprintln(w, "updateUserinfo Page")
+		fmt.Fprintln(w, "updateUserinfo Page")
+	req.ParseForm()
+	username := req.FormValue("username")
+	password := req.FormValue("password")
+	bloodType := req.FormValue("bloodType")
+	contactNumber := req.FormValue("contactNumber")
+	fmt.Fprintln(w, "username:", username, "password:", password, "bloodType: ",bloodType, "contactNumber:",contactNumber)
+	hashedPassword, err := HashPassword(password)
+	checkError(err)
+	
+	filter := bson.D{{"Username", username}}
+	
+
+	// Insert one
+	res, err := col.UpdateOne(ctx, filter, &Post{
+		Username :     username,          
+		Password :     hashedPassword,            
+		BloodType :    bloodType,   
+		ContactNumber: contactNumber, 
+		Tag : "bloodDonors"})
+	checkError(err)
+
+	if err == nil {
+		fmt.Printf("inserted id: %s\n", res.InsertedID.(primitive.ObjectID).Hex())
+	}
+	
 }
 
 func findDonors(w http.ResponseWriter, req *http.Request) {
