2d143f4c7e96c7cef6c3d0e47cce41ecc49a6a07
Gandhar Deshpande <gandhard1@gmail.com>
2022-04-30 16:51:19 -0400

    Backend (#8)
    * updated login and register handlers for mongodb
    
    * Changes to be committed:
    	modified:   main.go
    
    * modified:   main.go
    
    * Update main.go
    
    * changed login status checking
    
    * fixed errors
    
    * Add files via upload
    
    * updated index.html and register.html
    
    * updated index.html and register.html
    
    * updated main
    
    * modified:   main.go
    	new file:   static/splash.html
    
    * new file:   static/requestblood.html
    	modified:   static/splash.html
    	modified:   static/index.html
    
    * modified:   main.go
    	modified:   static/index.html
    	modified:   static/update.html
    
    * updated main
    
    * fixed errors
    
    * updated requestBlood function and added html template impl for login
    
    * update
    
    * Add files via upload
    
    * updated main
    
    * Add files via upload
    
    * Add files via upload
    
    * Add files via upload
    
    * modified:   main.go
    
    * updated main
    
    * updated static
    
    * fixed issues with requestblood, added image for background
    
    * fixed issues and html updates
    
    * Delete requestblood.html
    
    * cleaned comments for merging into main
    
    Co-authored-by: Owen Heckmann <oheckman@uncc.edu>
    Co-authored-by: oheckmann <38300728+oheckmann@users.noreply.github.com>
    Co-authored-by: nikkipat97 <57465967+nikkipat97@users.noreply.github.com>

--- main.go
+++ main.go
@@ -1,60 +1,244 @@
 package main
 
 import (
+	"context"
 	"fmt"
+	"html/template"
 	"log"
 	"net/http"
-	"sync"
+	"time"
+
+	"go.mongodb.org/mongo-driver/bson"
+	"go.mongodb.org/mongo-driver/bson/primitive"
+	"go.mongodb.org/mongo-driver/mongo"
+	"go.mongodb.org/mongo-driver/mongo/options"
+	"golang.org/x/crypto/bcrypt"
 )
 
-var RWLock sync.RWMutex
+const (
+	mongodbEndpoint = "mongodb://localhost:27017"
+)
+
+type userData struct {
+	ID            primitive.ObjectID `bson:"_id"`
+	Username      string             `bson:"username"`
+	Password      string             `bson:"password"`
+	BloodType     string             `bson:"blood_type"`
+	ContactNumber string             `bson:"contact_number"`
+	Location      string             `bson:"location"`
+	DonationCount uint16             `bson:"donation_count"`
+	CreatedAt     time.Time          `bson:"created_at"`
+}
+
+var ctx context.Context
+var col *mongo.Collection
+var tpl *template.Template
+
+func checkError(err error) {
+	if err != nil {
+		log.Fatal(err)
+	}
+}
 
 func main() {
-	// db := database{"shoes": 50, "socks": 5}
+	client, err := mongo.NewClient(
+		options.Client().ApplyURI(mongodbEndpoint),
+	)
+	checkError(err)
+	// Connect to mongo
+	ctx = context.Background()
+	err = client.Connect(ctx)
+	checkError(err)
+	// Disconnect
+	defer client.Disconnect(ctx)
+
+	// select collection from database
+	col = client.Database("bloodBankDatabase").Collection("users")
+
+	tpl, _ = template.ParseGlob("static/*.html")
+
 	mux := http.NewServeMux()
 	mux.Handle("/", http.FileServer(http.Dir("static")))
 	mux.HandleFunc("/loginHandler", loginHandler)
-	mux.HandleFunc("/RegisterHandler", RegisterHandler)
+	mux.HandleFunc("/registerHandler", registerHandler)
 	mux.HandleFunc("/deleteUser", deleteUser)
 	mux.HandleFunc("/updateUserinfo", updateUserinfo)
-	mux.HandleFunc("/findDonors", findDonors)
-	mux.HandleFunc("/listDonors", listDonors)
-	log.Fatal(http.ListenAndServe(":8000", mux)) // Listens for curl communication of localhost
+	mux.HandleFunc("/listAllDonors", listAllDonors)
+	mux.HandleFunc("/requestBlood", requestBlood)
+	mux.HandleFunc("/makeDonation", makeDonation)
+	log.Fatal(http.ListenAndServe(":8000", mux))
+}
+
+func HashPassword(password string) (string, error) {
+	bytes, err := bcrypt.GenerateFromPassword([]byte(password), 14)
+	return string(bytes), err
+}
+
+func CheckPasswordHash(password, hash string) bool {
+	err := bcrypt.CompareHashAndPassword([]byte(hash), []byte(password))
+	return err == nil
+}
+
+type loginPageData struct {
+	Username string
 }
 
 func loginHandler(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "loginHandler Page")
 	req.ParseForm()
 	username := req.FormValue("username")
 	password := req.FormValue("password")
-	fmt.Fprintln(w, "username:", username, "password:", password)
+	data := loginPageData{username}
+	var hash string
+
+	filter := bson.M{"username": username}
+
+	var p userData
+	if err := col.FindOne(ctx, filter).Decode(&p); err != nil {
+		fmt.Fprintln(w, "user:", username, " is not registered")
+		return
+	} else {
+		fmt.Printf("userData: %+v\n", p)
+		hash = p.Password
+	}
+
+	ok := CheckPasswordHash(password, hash)
+	if !ok {
+		fmt.Fprintln(w, "username or password is incorrect!")
+	} else {
+		tpl.ExecuteTemplate(w, "splash.html", data)
+	}
+
 }
 
-// type database map[string]dollars // database of items with their polar prices
-func Register(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "Register Page")
-}
-
-func RegisterHandler(w http.ResponseWriter, req *http.Request) {
+func registerHandler(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, "RegisterHandler Page")
-}
+	req.ParseForm()
+	username := req.FormValue("username")
+	password := req.FormValue("password")
+	contactNumber := req.FormValue("contact")
+	bloodType := req.FormValue("bloodtype")
+	location := req.FormValue("locations")
+	fmt.Println(w, "username:", username, "password:", password, "bloodType: ", bloodType, "contactNumber:", contactNumber, "location:", location)
 
-func addUser(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "addUser Page")
+	//check if username is available
+	filter := bson.M{"username": username}
+	var p userData
+	if err := col.FindOne(ctx, filter).Decode(&p); err == nil {
+		fmt.Fprintln(w, "username:", username, " is not available, please try different username")
+		return
+	}
+
+	hashedPassword, err := HashPassword(password)
+	checkError(err)
+
+	res, err := col.InsertOne(ctx, &userData{
+		ID:            primitive.NewObjectID(),
+		Username:      username,
+		Password:      hashedPassword,
+		BloodType:     bloodType,
+		ContactNumber: contactNumber,
+		Location:      location,
+		CreatedAt:     time.Now(),
+		DonationCount: 0,
+	})
+
+	if err == nil {
+		fmt.Printf("inserted id: %s\n", res.InsertedID.(primitive.ObjectID).Hex())
+		fmt.Fprintln(w, "user:", username, " is registered successfully!")
+	} else {
+		fmt.Fprintln(w, "Error in Registration, Please try again")
+	}
+
 }
 
 func deleteUser(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "deleteUser Page")
+	username := req.FormValue("username")
+	fmt.Fprintln(w, "username: ", username)
+	res, err := col.DeleteMany(ctx, bson.M{"username": username})
+	if err != nil {
+		log.Fatal(err)
+	} else if res.DeletedCount == 0 {
+		fmt.Fprintln(w, "Account is already deleted!")
+	} else if res.DeletedCount > 0 {
+		fmt.Fprintln(w, "Account Deleted Successfully ")
+	}
 }
 
 func updateUserinfo(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "updateUserinfo Page")
+	req.ParseForm()
+	username := req.FormValue("username")
+	password := req.FormValue("password")
+	contactNumber := req.FormValue("contactNumber")
+	location := req.FormValue("locations")
+	fmt.Println("username", username, "password:", password, "contactNumber:", contactNumber, "location:", location)
+	hashedPassword, err := HashPassword(password)
+	checkError(err)
+
+	filter := bson.D{{"username", username}}
+
+	res, err := col.UpdateOne(ctx, filter, &userData{
+		Password:      hashedPassword,
+		ContactNumber: contactNumber,
+		Location:      location,
+	})
+
+	if err == nil {
+		fmt.Println("update count: ", res.ModifiedCount)
+		fmt.Fprintln(w, "Update Successful!")
+	} else {
+		fmt.Fprint(w, "Can't Update, Please try again")
+	}
+
 }
 
-func findDonors(w http.ResponseWriter, req *http.Request) {
-	fmt.Fprintln(w, "findDonors Page")
-}
-
-func listDonors(w http.ResponseWriter, req *http.Request) {
+func listAllDonors(w http.ResponseWriter, req *http.Request) {
 	fmt.Fprintln(w, "listDonors Page")
+}
+
+func requestBlood(w http.ResponseWriter, req *http.Request) {
+	req.ParseForm()
+	location := req.FormValue("locations")
+	bloodType := req.FormValue("bloodtype")
+
+	filter := bson.M{"blood_type": bloodType, "location": location}
+
+	var donorInfo userData
+	curr, err := col.Find(ctx, filter)
+	defer curr.Close(ctx)
+	if err != nil {
+		fmt.Fprintln(w, "No results found for requested search criteria")
+		return
+	} else {
+		fmt.Fprintln(w, "Blood Donors Available, Please find details below:")
+		for curr.Next(ctx) {
+			err := curr.Decode(&donorInfo)
+			if err != nil {
+				fmt.Fprintln(w, "Error in Decoding")
+				return
+			} else if donorInfo.DonationCount > 0 {
+				fmt.Fprintln(w, "---------------------+---------------------")
+				fmt.Fprintln(w, "Contact:", donorInfo.ContactNumber, "Location:", donorInfo.Location)
+			} else {
+				fmt.Fprintln(w, "No results found for requested search criteria")
+			}
+		}
+	}
+
+}
+
+func makeDonation(w http.ResponseWriter, req *http.Request) {
+
+	req.ParseForm()
+	username := req.FormValue("username")
+	update := bson.M{"$inc": bson.M{"donation_count": +1}}
+
+	var donorInfo userData
+
+	err := col.FindOneAndUpdate(ctx, bson.M{"username": username}, update).Decode(&donorInfo)
+	if err != nil {
+		fmt.Fprintln(w, "Could not update donation count for user:", username)
+	} else {
+		fmt.Fprintln(w, "make donation request successful! \nupdated donation count for ", username, " : ", donorInfo.DonationCount)
+	}
+
 }
